98b9264fa84cebf815027ab8244b4a06592a8a97
ISABELLA HILARIA WRUCK <iwruck@labcetec.net>
2024-09-13 16:46:50 -0300

    Adicionando Css ao React 2

--- src/components/ComponentORemake.jsx
+++ src/components/ComponentORemake.jsx
@@ -3,7 +3,7 @@ import styles from "./ComponentORemake.module.css"
 const ComponentORemake = () => {
   return (
     <div>
-        <h1 className={styles.my_title}>TEXTO DO REMAKE FINAL HD 4K</h1>
+        <h1 className={styles.my_title}>TEXTO DO REMAKE FINAL HD MASTER 4K</h1> 
     </div>
   )
 }
